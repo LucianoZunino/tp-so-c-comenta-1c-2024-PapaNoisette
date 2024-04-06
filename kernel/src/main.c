c4ad22283462401902aadf9715cb34f67a8ab31d
FedericoBruni <fbruni@frba.utn.edu.ar>
2024-04-06 16:07:47 -0300

    Agregadas las utils del tp0 y los archivos config

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -1,8 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <utils/hello.h>
+#include <utils/utils.h>
 
 int main(int argc, char* argv[]) {
     decir_hola("Kernel");
+    t_log* logger_kernel = iniciar_logger("kernel.log", "Kernel");
+    t_config* config_kernel = iniciar_config("kernel.config");
+
+    char* ip_memoria = config_get_string_value(config_kernel, "IP_MEMORIA");
+	char* ip_cpu = config_get_string_value(config_kernel, "IP_CPU");
+    char* puerto_escucha = config_get_string_value(config_kernel, "PUERTO_ESCUCHA");
+	
+	log_info(logger_kernel, "Leí la IP_MEMORIA %s, la IP_CPU %s, y el PUERTO_ESCUCHA %s\n", ip_memoria, ip_cpu, puerto_escucha);
     return 0;
 }
---
+++ utils/src/utils/utils.h
@@ -0,0 +1,23 @@
+#ifndef UTILS_H_
+#define UTILS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include<signal.h>
+#include<unistd.h>
+#include<sys/socket.h>
+#include<netdb.h>
+#include<string.h>
+#include <commons/log.h>
+#include <commons/config.h>
+
+t_config* iniciar_config(char* ruta_config);
+t_log* iniciar_logger(char* ruta_logger, char* nombre_logger);
+int crear_conexion(char *ip, char* puerto);
+void liberar_conexion(int socket_cliente);
+int iniciar_servidor(char* ip, char* puerto, t_log* logger);
+int esperar_cliente(int socket_servidor, t_log* logger);
+int recibir_operacion(int socket_cliente);
+
+
+#endif
