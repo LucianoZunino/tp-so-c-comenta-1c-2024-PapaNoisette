7b7d65d320219bffdc1d8391a4a74fa604d3eaf9
ignaciofmunoz <ignaciofmunoz@github.com>
2024-05-08 21:33:00 +0000

    seguimos con cpu resolviendo instrucciones

--- cpu/src/utils.h
+++ cpu/src/utils.h
@@ -7,6 +7,7 @@
 #include <ctype.h>
 #include <string.h>
 #include <utils/utils.h>
+#include <main.h>
 
 typedef enum {
     SET,  
@@ -28,32 +29,13 @@ typedef enum {
     IO_FS_WRITE,
     IO_FS_READ,
     EXIT
-} ex_instruction;
-
-/*SET SET 
-MOV_IN 
-MOV_OUT
-SUM 
-SUB
-INZ
-RESIZE
-COPY_STRING
-WAIT
-SIGNAL
-IO_GEN_SLEEP
-IO_STDIN_READ
-IO_STDOUT_WRITE
-IO_FS_CREATE
-IO_FS_DELETE
-IO_FS_TRUNCATE
-IO_FS_WRITE
-IO_FS_READ
-EXIT*/
+} t_instruction_code;
 
 //void cambiar_contexto(void* buffer);   // Setea los registros y demas estructuras del CPU con los datos recibidos de un proceso
-//ex_instruction decode(t_instruccion* instruccion); 
-ex_instruction decode(char* instruccion);// Toma una instruccion y devuelve a que elemento del enum as_instruction corresponde
+//instruction_code decode(t_instruccion* instruccion); 
+t_instruction_code decode(char* instruccion);// Toma una instruccion y devuelve a que elemento del enum as_instruction corresponde
 //cod_op_kernel ejecutar_instrucciones(); // Ejecuta instrucciones hasta  que se encuentra con alguna que requiere terminar la ejecucion (break)
+void ejecutar_instruccion(); // Ejecuta instrucciones hasta  que se encuentra con alguna que requiere terminar la ejecucion (break)
 //void imprimir_contexto_actual();
 
 #endif
